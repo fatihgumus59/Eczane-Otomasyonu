84cf3fe30bb75a9d4666e447f0706c56cf3186e6
fatihgumus59 <fatihgumus72@gmail.com>
2022-05-05 14:07:00 +0300

    make it better

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.route('/').get(pendingMiddleware,pageController.getIndexPage); // hem giriş kontrolü hemde eğer kullanıcı rolü pending ise profil ekranına atacak
 router.route('/notlar').get(authMiddleware,pageController.getNotesPage);
 router.route('/yazdir/:id').get(authMiddleware,pageController.getProforma); // sadece yöneticilere açık fatura
-router.route('/invoice/:id').get(authMiddleware,pageController.getPublicProforma); // herkese açık fatura
+router.route('/invoice/:id').get(pageController.getPublicProforma); // herkese açık fatura
 router.route('/login').get(redirectMiddleware,pageController.getLoginPage);
 router.route('/register').get(redirectMiddleware,pageController.getRegisterPage);
 router.route('/profile').get(authMiddleware,pageController.getProfile);
